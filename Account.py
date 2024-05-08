0280f431cca9bdaf802e2dde65b1d76f134550a8
chotubaba <75783917+chotubaba@users.noreply.github.com>
2024-05-08 14:54:09 +0530

    Remove instructions from Account.py

--- Account.py
+++ Account.py
@@ -29,6 +29,8 @@ class Account:
         
         history_message = HistoryMessages.deposit(status, amount, self.balance)
         self.write_to_history(history_message)
+        # deposit is a @static method in HistoryMessages.
+        # so it can be called by HistoryMessages.deposit() without creating instance of History messages class
 
 
     def debit(self, amount):
@@ -45,27 +47,15 @@ class Account:
         except ValueError:
             print("Invalid input for debit!")
             status = "failure"
+        
 
         history_message = HistoryMessages.debit(status, amount, self.balance)
         self.write_to_history(history_message)
 
-        # TODO:
-        # implement account debits with all necessary checks
-        # amount must be a integer greater than 0
-        # if amount is greater than the amount in the account (insufficient funds) the operation should not work
-
-        # in case of positive outcome use this construct to write to JSON file
-
-        # history_message = HistoryMessages.debit("success", amount, self.balance)
-        # self.write_to_history(history_message)
-
-        # in case of a negative outcome, use this construct to write to a JSON file
-        
-        # history_message = HistoryMessages.debit("failure", amount, self.balance)
-        # self.write_to_history(history_message)
 
     def get_balance(self):
         return self.balance
+
 
     def dict_to_string(self, dict):
         if dict["operation_type"] != "exchange":
@@ -77,7 +67,3 @@ class Account:
     def get_history(self):
         history = self.file_manager.read_json(self.hist_file_path)
         return history
-
-        # TODO:
-        # implement a process that returns transaction history line by line
-        # use the dict_to_string method to create a string from a dictionary
